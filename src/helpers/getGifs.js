8e1031b4862e2da03bb5670c3a89294e2f21f8db
AlanSanzana <69324972+AlanSanzana@users.noreply.github.com>
2024-07-22 18:13:35 -0400

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async(category) => {
+
+    const apiKey = 'tZ3KqipRkmJhoGquCxUrwAmuFKR03aTo';
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=10`;
+    
+    const response = await fetch(url);
+
+    const { data } = await response.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+}
